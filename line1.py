bf9dab01e0416dc640d5790a8d41bd487feda6f2
Ayesha Irshad <ayeshaitshad124@gmail.com>
2023-12-27 21:13:14 -0700

    Improvement

--- line1.py
+++ line1.py
@@ -4,9 +4,15 @@ def find_slope_and_intercept(x1, y1, x2, y2):
     b = y1 - m * x1
     return m, b
 
-def find_intersection_point(line1, line2):
-    x1, y1, x2, y2 = line1
-    x3, y3, x4, y4 = line2
+def find_intersection_point(line1x1, line1x2, line1y1, line1y2, line2x1, line2x2, line2y1, line2y2):
+    x1=line1x1
+    x2=line1x2
+    y1=line1y1
+    y2=line1y2
+    x3=line2x1
+    x4=line2x2
+    y3=line2y1
+    y4=line2y2
 
 
     m1, b1 = find_slope_and_intercept(x1, y1, x2, y2)
@@ -23,10 +29,15 @@ def find_intersection_point(line1, line2):
 
 
 
-def plot_lines_and_intersection(line1, line2, intersection_point):
-    x1, y1, x2, y2 = line1
-    x3, y3, x4, y4 = line2
-
+def plot_lines_and_intersection(line1x1, line1x2, line1y1, line1y2, line2x1, line2x2, line2y1, line2y2, intersection_point):
+    x1=line1x1
+    x2=line1x2
+    y1=line1y1
+    y2=line1y2
+    x3=line2x1
+    x4=line2x2
+    y3=line2y1
+    y4=line2y2
     plt.plot([x1, x2], [y1, y2], label='Line 1')
     plt.plot([x3, x4], [y3, y4], label='Line 2')
 
--- menu.py
+++ menu.py
@@ -1,3 +1,4 @@
+import re
 import line1
 import line2
 import line3 
@@ -6,78 +7,26 @@ import grahamscan
 import jarvismarch
 import quickelimination
 import quickandgrahamcombine
+from flask import Flask, request
+app = Flask(__name__)
 
-print('enter 1 for Intersection of  line and 2 for convexhull')
-num = int(input())
-if num==1:
-    print('Intersection of Lines')
-    print('enter 1 to find intersection by algebra')
-    print('enter 2 to find intersection by Orientation')
-    print('enter 3 to find intersection by Vectorisation')
-    num = int(input())
-    if num==1:
-        print('enter line one in this format (X1,Y1,X2,Y2)')
-        lineone = input()
-        numbers = lineone[1:-1].split(',')
-        lineone=tuple(map(int,numbers))
-        
-        print('enter line 2 in this format (X1,Y1,X2,Y2)')
-        linetwo= input()
-        numbers = linetwo[1:-1].split(',')
-        linetwo = tuple(map(int,numbers))
-        intersection_point =   line1.find_intersection_point(lineone, linetwo)
-        print("Intersection Point:", intersection_point)
-        line1.plot_lines_and_intersection(lineone, linetwo, intersection_point)
-    elif num==2:
-         
-         print('enter line one in this format (X1,Y1,X2,Y2)')
-         line_input = input("Enter line in this format (X1, y1, x2, y2): ")
-         numbers = line_input[1:-1].split(',')
-         x1, y1, x2, y2 = map(float, numbers)
-         point1 = line2.Point(x1, y1)
-         point2 = line2.Point(x2, y2)
-         line_input2 = input("Enter line in this format (X1, y1, x2, y2): ")
-         numbers = line_input2[1:-1].split(',')
-         x3, y3, x4, y4 = map(float, numbers)
-         point3 = line2.Point(x3, y3)
-         point4 = line2.Point(x4, y4)
+@app.route('/#algebra', methods=['POST'])
+def algebra():
+    # Extract values from form data
+    line1Point1X = request.json.get('line1').get('point1').get('x')
+    line1Point1Y = request.json.get('line1').get('point1').get('y')
+    line1Point2X = request.json.get('line1').get('point2').get('x')
+    line1Point2Y = request.json.get('line1').get('point2').get('y')
+    line2Point1X = request.json.get('line2').get('point1').get('x')
+    line2Point1Y = request.json.get('line2').get('point1').get('y')
+    line2Point2X = request.json.get('line2').get('point2').get('x')
+    line2Point2Y = request.json.get('line2').get('point2').get('y')
 
-         lineone = line2.Line(point1, point2)
-         linetwo = line2.Line(point3, point4)
-         line2.IntersectbyOrient(lineone,linetwo)
-    elif num==3:
-         print('enter line one in this format (X1,Y1,X2,Y2)')
-         line_input = input("Enter line in this format (X1, y1, x2, y2): ")
-         numbers = line_input[1:-1].split(',')
-         x1, y1, x2, y2 = map(float, numbers)
-         point1 = line3.Point(x1, y1)
-         point2 = line3.Point(x2, y2)
-         line_input2 = input("Enter line in this format (X1, y1, x2, y2): ")
-         numbers = line_input2[1:-1].split(',')
-         x3, y3, x4, y4 = map(float, numbers)
-         point3 = line3.Point(x3, y3)
-         point4 = line3.Point(x4, y4)
+    # TODO: Add your algebra code here
+    # For example, you can call a function from quickandgrahamcombine
+    result = line1.find_intersection_point(line1Point1X, line1Point2X, line1Point1Y, line1Point2Y, line2Point1X, line2Point2X, line2Point1Y, line2Point2Y)
+    line1.plot_lines_and_intersection(line1Point1X, line1Point2X, line1Point1Y, line1Point2Y, line2Point1X, line2Point2X, line2Point1Y, line2Point2Y, result)
 
-         lineone = line3.Line(point1, point2)
-         linetwo = line3.Line(point3, point4)
-         line3.Vectorisation(lineone,linetwo)
-             
-elif num==2:
-    print('Finding Convex hull by points')
-    print('enter 1 for Bruteforce')
-    print('enter 2 for Grahamscan')
-    print('enter 3 for jarvismarch')
-    print('enter 4 for QuickElimination')
-    print('enter 5 for Researchbased')
-    num = int(input())
-    if num==1:
-        bruteforce.InputandStart()
-    elif num==2:
-        grahamscan.InputandStart()
-    elif num==3:
-        jarvismarch.InputandStart()
-    elif num==4:
-        quickelimination.InputandStart()
-    elif num==5:
-         quickandgrahamcombine.InputandStart()
-                    
+
+if __name__ == '__main__':
+    app.run(debug=True)
